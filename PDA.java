dfd34afc124e31c5f85815a5f60f2f1913aec1cf
Simon Pak <simon.pak@mrjaffesclass.com>
2023-11-06 11:00:31 -0800

    PDA step 5

---
+++ PDA.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+/**
+ * Permissible Dating Age program
+ *
+ * @author Mr. Jaffe
+ * @version 2021-06-22 Version 1.0.0
+ */
+public class PDA
+{
+
+    /**
+     * Constructor for objects of class PDA
+     */
+    public PDA()
+    {
+        // We don't need to do anything in the constructor for
+        // our program.
+    }
+    
+    /**
+     * This is the main event loop for our PDA program
+     */
+    public void runEventLoop() {
+    Scanner scanner = new Scanner(System.in);
+    int LOWER_BOUND = 0;
+    while (true) {
+        System.out.println("How old are you?");
+       try {
+            int age= scanner.nextInt();
+           if (age < LOWER_BOUND){
+               System.out.println(age+" is too young!!");
+            }else{
+                System.out.println("Computations go here");
+            }
+       }  catch (InputMismatchException error) {
+           scanner.next();
+           System.out.println("Please enter an integer");
+       }
+    }
+}   
+/**
+     * The main method instantiates and runs the program
+     */
+    public static void main (String[] args) {
+        PDA pda = new PDA();
+        pda.runEventLoop();
+    }
+}
